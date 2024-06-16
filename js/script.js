a744d61b867976869b7e28cc11a7b69d70ab04a6
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-06-16 14:35:14 -0300

    edit todo done

--- js/script.js
+++ js/script.js
@@ -41,6 +41,24 @@ const saveTodo = (text) => {
 
   todoInput.focus();
 };
+
+const toggleForms = () => {
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+};
+
+const upadteTodo = (text) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+    }
+  });
+};
 // Eventos
 
 todoForm.addEventListener("submit", (e) => {
@@ -56,6 +74,11 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
   const targetEl = e.target;
   const parentEl = targetEl.closest("div");
+  let todoTitle;
+
+  if (parentEl && parentEl.querySelector("h3")) {
+    todoTitle = parentEl.querySelector("h3").innerText;
+  }
 
   if (targetEl.classList.contains("finish-todo")) {
     parentEl.classList.toggle("done");
@@ -66,5 +89,27 @@ document.addEventListener("click", (e) => {
   }
 
   if (targetEl.classList.contains("edit-todo")) {
+    toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
   }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if (editInputValue) {
+    upadteTodo(editInputValue);
+  }
+
+  toggleForms();
 });
